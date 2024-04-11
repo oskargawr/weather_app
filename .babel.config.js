6deeb4478d65d7ae87451d37a5855eba0844ba5f
oskargawr <oskargawryszewski1@gmail.com>
2024-04-11 20:58:30 +0200

    still working on fixing issues

--- .babel.config.js
+++ .babel.config.js
@@ -1,4 +1,7 @@
 module.exports = {
-  presets: ["@babel/preset-env", "@babel/preset-react"],
-  plugins: ["@babel/plugin-proposal-private-property-in-object"],
+  presets: ["@babel/preset-env", "@babel/preset-react", "@babel/preset-flow"],
+  plugins: [
+    "babel-plugin-styled-components",
+    "@babel/plugin-proposal-class-properties",
+  ],
 };
